d53a73664ce3c390cc2085e75ad889e747e1826d
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-06-16 01:54:10 +0600

    shopping cart handle decrease and update price

---
+++ js/case.js
@@ -0,0 +1,49 @@
+
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberFieldString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberFieldString);
+    let newCaseNumber;
+    if(isIncrease == true){
+        newCaseNumber = previousCaseNumber + 1;
+    } else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+   
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;
+}
+function updateTotalCasePrice(newCaseNumber){
+          const caseTotalPrice = newCaseNumber * 50;
+          const totalCasePriceElement = document.getElementById('case-total');
+          totalCasePriceElement.innerText = caseTotalPrice;
+}
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(true);
+    updateTotalCasePrice(newCaseNumber);
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(false);
+    updateTotalCasePrice(newCaseNumber);
+})
+
+
+// document.getElementById('btn-case-plus').addEventListener('click', function(){
+//     const caseNumberField = document.getElementById('case-number-field');
+//     const caseNumberFieldString = caseNumberField.value;
+//     const previousCaseNumber = parseInt(caseNumberFieldString);
+//     const newCaseNumber = previousCaseNumber + 1;
+//     caseNumberField.value = newCaseNumber;
+// })
+
+
+// document.getElementById('btn-case-minus').addEventListener('click', function(){
+//     const caseNumberField = document.getElementById('case-number-field');
+//     const caseNumberFieldString = caseNumberField.value;
+//     const previousCaseNumber = parseInt(caseNumberFieldString);
+//     const newCaseNumber = previousCaseNumber - 1;
+//     caseNumberField.value = newCaseNumber;
+
+// })
