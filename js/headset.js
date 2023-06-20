599dec731b148afc80887aff96737d6d2d94fa8b
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-06-20 18:01:33 +0600

    calculate the first part of subtotal section

---
+++ js/headset.js
@@ -0,0 +1,29 @@
+function updateHeadsetNumber(isIncrease){
+    const headsetNumberField = document.getElementById('headset-number-field');
+    const headsetNumberString = headsetNumberField.value;
+    const previousHeadsetNumber = parseInt(headsetNumberString);
+    let newHeadsetNumber;
+
+    if(isIncrease == true){
+        newHeadsetNumber = previousHeadsetNumber + 1;
+    }else{
+        newHeadsetNumber = previousHeadsetNumber - 1;
+    }
+    headsetNumberField.value = newHeadsetNumber;
+    return newHeadsetNumber;
+}
+function updateTotalHeadsetPrice(newHeadsetNumber){
+    const totalHeadsetPrice = newHeadsetNumber * 60;
+    const headsetTotalElement = document.getElementById('headset-total');
+    headsetTotalElement.innerText = totalHeadsetPrice;
+}
+
+document.getElementById('btn-headset-plus').addEventListener('click', function(){
+    const  newHeadsetNumber = updateHeadsetNumber(true);
+    updateTotalHeadsetPrice(newHeadsetNumber);
+})
+
+document.getElementById('btn-headset-minus').addEventListener('click', function(){
+   const  newHeadsetNumber = updateHeadsetNumber(false);
+   updateTotalHeadsetPrice(newHeadsetNumber);
+})
--- js/phone.js
+++ js/phone.js
@@ -16,18 +16,41 @@ function updatePhoneNumber(isIncrease){
     phoneNumberField .value =  newPhoneNumber;
     return newPhoneNumber;
 }
+function getTextElementById(elementId){
+               //calculate total
 
+               const phoneTotalElement =document.getElementById(elementId);
+               const currentPhoneTotalString = phoneTotalElement.innerText;
+               const currentPhoneTotal = parseInt(currentPhoneTotalString );
+               return currentPhoneTotal;
+            //   const phoneTotalElement = document.getElementById('elementId');
+            //   const currentPhoneTotalString = phoneTotalElement.innerText;
+            //   const currentPhoneTotal = parseInt(currentPhoneTotalString);
+             
+}
 function updatePhoneTotalPrice(newPhoneNumber){
     const phoneTotalPrice = newPhoneNumber * 1200;
     const phoneTotalElement = document.getElementById('phone-total');
     phoneTotalElement.innerText = phoneTotalPrice;
+        
+
+  
 }
 
 document.getElementById('btn-phone-plus').addEventListener('click', function(){
   
    const newPhoneNumber = updatePhoneNumber(true);
    updatePhoneTotalPrice(newPhoneNumber);
-  
+   // calculate sub-total
+
+   const currentPhoneTotal = getTextElementById('phone-total');
+
+   const currentCaseTotal = getTextElementById('case-total');
+   const currentHeadsetTotal = getTextElementById('headset-total');
+   const currentSubTotal =  currentPhoneTotal + currentCaseTotal + currentHeadsetTotal;
+
+   const subTotalElement = document.getElementById('sub-total');
+   subTotalElement.innerText = currentSubTotal;
 })
 
 
